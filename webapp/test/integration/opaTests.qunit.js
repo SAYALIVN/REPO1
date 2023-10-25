e6ac3098a90b3c866637c6afae758db28168234a
Sayali Nemade <sayali.nemade@asint.net>
2023-10-25 10:43:51 +0000

    Sample  fiori app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["buttondemo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"buttondemo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
